d33845e943a8d0eb9af0f64cf68168c8c3835e81
Enzo-Ar <enzoarmelinbarros@gmail.com>
2026-09-15 20:28:05 -0300

    Mudança Pequena
    Só adicionei o botão de login no index

---
+++ public/js/index.js
@@ -0,0 +1,19 @@
+document.addEventListener('readystatechange', (event) => {
+    if (event.target.readyState === 'complete') {
+        init();
+    }
+});
+
+const init = async () => {
+    const navList = document.querySelector('#navList');
+    const log_sign = document.createElement('li');
+    log_sign.className = 'nav-item';
+
+    const log_sign_href = document.createElement('a');
+    log_sign_href.className = 'nav-link';
+    log_sign_href.href = '/login';
+    log_sign_href.textContent = 'login';
+
+    log_sign.appendChild(log_sign_href);
+    navList.appendChild(log_sign);
+}
